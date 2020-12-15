488ee09f490aef5935b6be8ce42105705ceb0cdf
billecr <billecr@amazon.com>
2020-12-15 13:52:14 +0100

    Add work folder with template for prediction

---
+++ work/predict_handler.py
@@ -0,0 +1,19 @@
+import logging as log
+import datetime
+
+
+def predict(start_date_str: str,
+            end_date_str: str,
+            fn_in: str,
+            fn_out) -> None:
+    # configure logging
+    log.basicConfig(filename='test.log', level=log.INFO)
+
+    # perform sanity checks
+    log.info(f"predict for {start_date_str} to {end_date_str} with file {fn_in} and output file {fn_out}")
+    start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
+    end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
+    log.info(f"parsed start date as: {start_date}")
+    log.info(f"parsed end date as: {end_date}")
+    log.info(f"reading input file {fn_in}")
+    return None
---
+++ work/predict_test.py
@@ -0,0 +1,6 @@
+import predict as predict
+
+
+predict.predict("2020-01-01", "2020-02-02", "input_file", "output_file")
+
+
