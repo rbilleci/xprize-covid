e5f27675312fa56055213809ed823fe698ba838f
billecr <billecr@amazon.com>
2020-12-15 13:54:44 +0100

    Add date/time to logging format

--- work/predict_handler.py
+++ work/predict_handler.py
@@ -7,7 +7,7 @@ def predict(start_date_str: str,
             fn_in: str,
             fn_out) -> None:
     # configure logging
-    log.basicConfig(filename='test.log', level=log.INFO)
+    log.basicConfig(filename='test.log', level=log.INFO, format='%(asctime)s %(filename)s: %(message)s')
 
     # perform sanity checks
     log.info(f"predict for {start_date_str} to {end_date_str} with file {fn_in} and output file {fn_out}")
