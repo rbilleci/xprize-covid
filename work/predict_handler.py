d2e0ad458ae076e62a8ee5271d465831b44ac105
billecr <billecr@amazon.com>
2020-12-15 14:18:28 +0100

    update logging

--- work/predict_handler.py
+++ work/predict_handler.py
@@ -1,15 +1,19 @@
-import logging as log
 import datetime
+import logging as log
+
+
+def initialize() -> None:
+    log.basicConfig(filename='predictions.log',level=log.INFO, format='%(asctime)s %(filename)s: %(message)s')
 
 
 def predict(start_date_str: str,
             end_date_str: str,
             fn_in: str,
             fn_out) -> None:
-    # configure logging
-    log.basicConfig(filename='test.log', level=log.INFO, format='%(asctime)s %(filename)s: %(message)s')
+    # Initialize
+    initialize()
 
-    # perform sanity checks
+    # Perform sanity checks
     log.info(f"predict for {start_date_str} to {end_date_str} with file {fn_in} and output file {fn_out}")
     start_date = datetime.datetime.strptime(start_date_str, '%Y-%m-%d').date()
     end_date = datetime.datetime.strptime(end_date_str, '%Y-%m-%d').date()
--- work/predict_test.py
+++ work/predict_test.py
@@ -2,5 +2,8 @@ import predict as predict
 
 
 predict.predict("2020-01-01", "2020-02-02", "input_file", "output_file")
+predict.predict("2020-01-01", "2020-02-02", "input_file", "output_file")
+predict.predict("2020-01-01", "2020-02-02", "input_file", "output_file")
+predict.predict("2020-01-01", "2020-02-02", "input_file", "output_file")
 
 
