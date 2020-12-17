0e75fc31a7bb1e15b4d7e04ea4823327f880e023
rbilleci <rick.billeci@gmail.com>
2020-12-17 15:38:14 +0100

    add additional test cases for different date ranges

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,6 +1,6 @@
 import os
 import unittest
-
+import covid_constants
 import predict as predict
 
 
@@ -11,5 +11,19 @@ class TestPredict(unittest.TestCase):
             os.chdir("../")
 
     def test_predict(self):
-        # TODO: test with different date ranges to simulate unexpected problems
-        predict.predict("2020-12-30", "2021-07-10", "data/future_ip.csv", "output_file.log")
+        # test a single day in the past
+        self.predict_for_range("2020-10-01", "2020-10-01")
+        # test two days in the past
+        self.predict_for_range("2020-10-01", "2020-10-02")
+        # test a single day in the future
+        self.predict_for_range("2020-12-30", "2020-12-30")
+        self.predict_for_range("2021-01-01", "2021-01-01")
+        # test a two days in the future, with the calendar year switchover
+        self.predict_for_range("2020-12-31", "2021-01-01")
+        # test the range we will use for the challenge
+        self.predict_for_range("2020-12-22", "2021-06-19")
+
+    @staticmethod
+    def predict_for_range(start_date, end_date):
+        predict.predict(start_date, end_date, covid_constants.path_data_future,
+                        f"tests/data_test_predictions_{start_date}_{end_date}.log")
