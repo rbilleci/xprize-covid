44f916c3c66872dee9b4916ed99a1ef6366589cd
billecr <billecr@amazon.com>
2020-12-19 23:08:55 +0100

    fix sandbox files

--- sandbox_package.sh
+++ sandbox_package.sh
@@ -1,4 +1,4 @@
 rm work.tar.gz
 rm *.log
-tar --exclude=./venv --exclude=./.idea --exclude=ml_trainer.py --exclude=work.tar.gz --exclude-vcs  -zcvf work.tar.gz .
+tar --exclude=./venv --exclude=./.git --exclude=./.idea --exclude=ml_trainer.py --exclude=work.tar.gz -zcvf work.tar.gz .
 tar -tf work.tar.gz
