80a1e7cf702c72429afb3d814b249b484199bf71
billecr <billecr@amazon.com>
2020-12-20 11:44:36 +0100

    update date, fix updat script

--- sandbox_update.sh
+++ sandbox_update.sh
@@ -1,2 +1,2 @@
-find * -not -path "predictions*" -not -path bootstrap.sh -not -path work.tar.gz -delete
+find * -not -path "predictions*" -not -path bootstrap.sh -not -path "sandbox_*" -not -path work.tar.gz -delete
 tar xvfz work.tar.gz
