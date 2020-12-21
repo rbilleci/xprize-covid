2ea2262ec9bd38a2d40b7a24edc3966430834a18
billecr <billecr@amazon.com>
2020-12-21 23:47:17 +0100

    submission

--- ml_trainer_walk.py
+++ ml_trainer_walk.py
@@ -3,30 +3,33 @@ from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.python.keras.layers import Dense, LeakyReLU, Dropout, PReLU, ELU
 from tensorflow.python.keras.models import Sequential
 import tensorflow as tf
+import numpy as np
 
+import constants
 import ml_splitter
 import ml_transformer
+from constants import PREDICTED_NEW_CASES
 from df_loader import load_ml_data
 from xlogger import log
 
 
 class HP:
     KERNEL_INITIALIZER = 'random_normal'  # 'random_normal'
-    OPTIMIZER = tf.keras.optimizers.Adam(learning_rate=0.0001)#)
+    OPTIMIZER = tf.keras.optimizers.Adam(learning_rate=0.0001)  # )
     METRICS = [tf.keras.metrics.RootMeanSquaredError()]
     LOSS = tf.keras.losses.MeanSquaredError()  # 'log_cosh'
     LAYER_SIZE = 200  # 200
     LAYERS = 2  # 2
     LAYER_DROPOUT = False
-    LAYER_DROPOUT_RATE = 0.25
+    LAYER_DROPOUT_RATE = 0.50
     OUTPUT_ACTIVATION = 'sigmoid'  # sigmoid
     DAYS_FOR_VALIDATION = 0  # 31
     DAYS_FOR_TEST = 10  # 14
     TRAINING_EPOCHS = 100
     TRAINING_BATCH_SIZE = 32
     VERBOSE = 2
-    EARLY_STOPPING_PATIENCE = 100
-    CALLBACKS = []#EarlyStopping(patience=EARLY_STOPPING_PATIENCE, restore_best_weights=True)]
+    EARLY_STOPPING_PATIENCE = 1000
+    CALLBACKS = []  # EarlyStopping(patience=EARLY_STOPPING_PATIENCE, restore_best_weights=True)]
 
 
 def get_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
@@ -106,8 +109,8 @@ def walk_and_chew_gum(model_name: str):
 
     model = get_model(train_and_validation.shape[1] - 1)
     records = train_and_validation.shape[0]
-    records_per_step = int(records / 100)
-    epochs_per_step = 2
+    records_per_step = int(records / 200)
+    epochs_per_step = 4
     i = 0
     while i < records:
         i += records_per_step
@@ -121,30 +124,11 @@ def walk_and_chew_gum(model_name: str):
                   batch_size=HP.TRAINING_BATCH_SIZE,
                   epochs=epochs_per_step,
                   callbacks=HP.CALLBACKS,
-                  verbose=1)
+                  verbose=0)
         # model.train_on_batch()
         # model.reset_states()
         loss_test = model.evaluate(test_x, test_y)
         log(f"loss, test: {loss_test}")
-
-
-"""
-    model = get_model(train_x.shape[1])
-    history = model.fit(train_x,
-                        train_y,
-                        validation_data=(validation_x, validation_y),
-                        batch_size=ml_trainer.HP.TRAINING_BATCH_SIZE,
-                        epochs=ml_trainer.HP.TRAINING_EPOCHS,
-                        callbacks=ml_trainer.HP.CALLBACKS,
-                        verbose=ml_trainer.HP.VERBOSE)
-    # Print data
-    best_epoch = np.argmin(history.history['val_loss'])
-    loss_train = history.history['loss'][best_epoch]
-    loss_validation = history.history['val_loss'][best_epoch]
-    loss_test = model.evaluate(test_x, test_y)
-    log(f"loss, training:{loss_train}")
-    log(f"loss, validation: {loss_validation}")
-    log(f"loss, test: {loss_test}")
 
     for i in range(0, 20):
         # expected value
@@ -159,8 +143,8 @@ def walk_and_chew_gum(model_name: str):
         expected = expected * constants.INPUT_SCALE[PREDICTED_NEW_CASES]
         predicted = predicted * constants.INPUT_SCALE[PREDICTED_NEW_CASES]
         log(f"EXPECTED/PREDICTED:\t{expected} vs {predicted}")
-    ml_trainer.save(model, model_name)
-    """
+    save(model, model_name)
+
 
 # Train to get the model for confirmed cases
 
@@ -168,4 +152,4 @@ pd.options.display.max_columns = 4
 pd.options.display.max_rows = 1000
 pd.options.display.max_info_columns = 1000
 
-walk_and_chew_gum("go-go-go")
+walk_and_chew_gum(PREDICTED_NEW_CASES)
