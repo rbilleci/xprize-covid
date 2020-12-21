6313cdb7f96b57a8524ead03365dc067a5d7f787
billecr <billecr@amazon.com>
2020-12-21 22:06:09 +0100

    a trained model

--- ml_trainer_walk.py
+++ ml_trainer_walk.py
@@ -1,40 +1,51 @@
-import numpy as np
 import pandas as pd
+from tensorflow.python.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.layers import Dense, LeakyReLU, Dropout, PReLU, ELU
+from tensorflow.python.keras.models import Sequential
 import tensorflow as tf
 
-import constants
 import ml_splitter
 import ml_transformer
-
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers.advanced_activations import ELU
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.advanced_activations import PReLU
-from keras.models import Sequential
-from tensorflow.python.keras.callbacks import EarlyStopping
 from df_loader import load_ml_data
-from constants import *
 from xlogger import log
 
 
 class HP:
     KERNEL_INITIALIZER = 'random_normal'  # 'random_normal'
-    OPTIMIZER = tf.keras.optimizers.Adam(learning_rate=0.0001)#)
+    OPTIMIZER = tf.keras.optimizers.Adadelta()
     METRICS = [tf.keras.metrics.RootMeanSquaredError()]
     LOSS = tf.keras.losses.MeanSquaredError()  # 'log_cosh'
-    LAYER_SIZE = 200  # 200
+    LAYER_SIZE = 500  # 200
     LAYERS = 2  # 2
     LAYER_DROPOUT = False
     LAYER_DROPOUT_RATE = 0.25
     OUTPUT_ACTIVATION = 'sigmoid'  # sigmoid
-    DAYS_FOR_VALIDATION = 31  # 31
+    DAYS_FOR_VALIDATION = 0  # 31
     DAYS_FOR_TEST = 10  # 14
     TRAINING_EPOCHS = 100
     TRAINING_BATCH_SIZE = 32
     VERBOSE = 2
     EARLY_STOPPING_PATIENCE = 100
     CALLBACKS = [EarlyStopping(patience=EARLY_STOPPING_PATIENCE, restore_best_weights=True)]
+
+
+def get_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    # load the data and perform the split
+    train_and_validation, _, test = ml_splitter.split(load_ml_data(), 0, HP.DAYS_FOR_TEST)
+    train_and_validation = ml_transformer.transform(train_and_validation, for_prediction=False)
+    test = ml_transformer.transform(test, for_prediction=False)
+    return train_and_validation, test
+
+
+def get_model_relu(model, dimensions) -> None:
+    for i in range(0, HP.LAYERS):
+        if i == 0:
+            model.add(Dense(HP.LAYER_SIZE, kernel_initializer=HP.KERNEL_INITIALIZER, input_dim=dimensions))
+        else:
+            model.add(Dense(HP.LAYER_SIZE, kernel_initializer=HP.KERNEL_INITIALIZER, activation='relu'))
+        # add dropout
+        if HP.LAYER_DROPOUT:
+            model.add(Dropout(HP.LAYER_DROPOUT_RATE))
 
 
 def get_model_elu(model, dimensions) -> None:
@@ -77,25 +88,11 @@ def get_model_lrelu(model, dimensions) -> None:
 
 def get_model(dimensions):
     model = Sequential()
-    get_model_prelu(model, dimensions)
+    get_model_relu(model, dimensions)
     model.add(Dense(1, kernel_initializer=HP.KERNEL_INITIALIZER, activation=HP.OUTPUT_ACTIVATION))
     model.compile(loss=HP.LOSS, optimizer=HP.OPTIMIZER, metrics=HP.METRICS)
     model.summary()
     return model
-
-
-def get_data() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
-    # load the data and perform the split
-    df_train, df_val, df_test = ml_splitter.split(load_ml_data(), HP.DAYS_FOR_VALIDATION, HP.DAYS_FOR_TEST)
-    # df_train, df_val, df_test = ml_splitter.split_random(load_ml_data())
-    # df_train, df_val, df_test = ml_splitter.split_random_with_reserved_test(load_ml_data(), HP.DAYS_FOR_TEST)
-
-    # transform the data for the neural network
-    tr = ml_transformer.transform(df_train, for_prediction=False).sample(frac=1).reset_index(drop=True)
-    val = ml_transformer.transform(df_val, for_prediction=False).sample(frac=1).reset_index(drop=True)
-    test = ml_transformer.transform(df_test, for_prediction=False).sample(frac=1).reset_index(drop=True)
-
-    return tr.iloc[:, 1:], tr.iloc[:, :1], val.iloc[:, 1:], val.iloc[:, :1], test.iloc[:, 1:], test.iloc[:, :1]
 
 
 def save(model, model_name: str):
@@ -103,17 +100,43 @@ def save(model, model_name: str):
     model.save(f"models/{model_name}", overwrite=True, include_optimizer=True, save_format='tf')
 
 
-def train(model_name: str):
-    train_x, train_y, validation_x, validation_y, test_x, test_y = get_data()
+def walk_and_chew_gum(model_name: str):
+    train_and_validation, test = get_data()
+    test_x, test_y = test.iloc[:, 1:], test.iloc[:, :1]
 
+    model = get_model(train_and_validation.shape[1] - 1)
+    records = train_and_validation.shape[0]
+    records_per_step = int(records / 100)
+    epochs_per_step = 2
+    i = 0
+    while i < records:
+        i += records_per_step
+        current_train, current_validation = train_and_validation[0:i], train_and_validation[i:i + records_per_step]
+        print('train=%d, validation=%d' % (len(current_train), len(current_validation)))
+        tx, ty = current_train.iloc[:, 1:], current_train.iloc[:, :1]
+        vx, vy = current_validation.iloc[:, 1:], current_validation.iloc[:, :1]
+        model.fit(tx,
+                  ty,
+                  validation_data=(vx, vy),
+                  batch_size=HP.TRAINING_BATCH_SIZE,
+                  epochs=epochs_per_step,
+                  callbacks=HP.CALLBACKS,
+                  verbose=1)
+        # model.train_on_batch()
+        # model.reset_states()
+        loss_test = model.evaluate(test_x, test_y)
+        log(f"loss, test: {loss_test}")
+
+
+"""
     model = get_model(train_x.shape[1])
     history = model.fit(train_x,
                         train_y,
                         validation_data=(validation_x, validation_y),
-                        batch_size=HP.TRAINING_BATCH_SIZE,
-                        epochs=HP.TRAINING_EPOCHS,
-                        callbacks=HP.CALLBACKS,
-                        verbose=HP.VERBOSE)
+                        batch_size=ml_trainer.HP.TRAINING_BATCH_SIZE,
+                        epochs=ml_trainer.HP.TRAINING_EPOCHS,
+                        callbacks=ml_trainer.HP.CALLBACKS,
+                        verbose=ml_trainer.HP.VERBOSE)
     # Print data
     best_epoch = np.argmin(history.history['val_loss'])
     loss_train = history.history['loss'][best_epoch]
@@ -136,8 +159,8 @@ def train(model_name: str):
         expected = expected * constants.INPUT_SCALE[PREDICTED_NEW_CASES]
         predicted = predicted * constants.INPUT_SCALE[PREDICTED_NEW_CASES]
         log(f"EXPECTED/PREDICTED:\t{expected} vs {predicted}")
-    save(model, model_name)
-
+    ml_trainer.save(model, model_name)
+    """
 
 # Train to get the model for confirmed cases
 
@@ -145,4 +168,4 @@ pd.options.display.max_columns = 4
 pd.options.display.max_rows = 1000
 pd.options.display.max_info_columns = 1000
 
-train(PREDICTED_NEW_CASES)
+walk_and_chew_gum("go-go-go")
